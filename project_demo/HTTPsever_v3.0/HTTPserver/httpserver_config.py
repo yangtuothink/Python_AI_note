aa0b09c9a956ad8f2b46d28be09405a0c302f2c3
yangtuo <745206110@qq.com>
2019-03-11 11:07:40 +0800

    增加了 __pycache__ 忽略

---
+++ project_demo/HTTPsever_v3.0/HTTPserver/httpserver_config.py
@@ -0,0 +1,20 @@
+"""
+HTTPserver 配置文件，　用户填写基本的必要信息
+"""
+
+# HTTPserver
+HOST = "0.0.0.0"
+PORT = 8000
+ADDR = (HOST, PORT)
+
+# debug 设置为　True 表示调试
+DEBUG = True
+
+# 配合　WebFrame　地址
+frame_ip = "127.0.0.1"
+frame_port = 8080
+frame_address = (frame_ip, frame_port)
+
+
+
+
